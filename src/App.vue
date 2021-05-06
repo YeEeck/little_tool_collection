e7a0ee48eb9cea47799d8892b1df34111ad08bcb
YeEeck <yechenkai@foxmail.com>
2021-05-07 00:34:17 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">CombinationNumberCal</router-link> |
+      <router-link to="/FS">FractionalSimplification</router-link>
     </div>
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import CombinationNumberCal from '../views/CombinationNumberCal.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'CombinationNumberCal',
+    component: CombinationNumberCal
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/FS',
+    name: 'FractionalSimplification',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/FractionalSimplification.vue')
   }
 ]
 
